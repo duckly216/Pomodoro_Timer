5c237763ac2f467374f9ae4d603551b581f659c1
duckly216 <jorgep020905@gmail.com>
2025-10-27 21:52:49 -0400

    Created total_duration member variable

--- src/Timer.h
+++ src/Timer.h
@@ -12,26 +12,27 @@ using namespace std::chrono;
 
 
 class Timer {
-    // Default time values are: 5 minutes for work, 2 minutes for break
-    // Default Cycles (times it goes through the work/break cycle)
+    // Minutes and seconds stored for pomodoro purposes
+    // Total duration is used for actual timer countdown (in seconds)
     minutes i_minutes{};
     seconds i_seconds{};
-    steady_clock::time_point current_time;
+    seconds total_duration{};
+    steady_clock::time_point start_time;
 
 public:
 
     Timer() {
         i_minutes = minutes(5);
         i_seconds = seconds(0);
+        total_duration = i_minutes + i_seconds;
     }
     Timer(int m, int s);
-
+    // Setters
     void set_all(int m, int s){
         this->i_minutes = minutes(m);
         this->i_seconds = seconds(s);
 
     }
-
     void set_minutes(int m) {
         this->i_minutes = minutes(m);
     }
@@ -39,13 +40,12 @@ public:
         this->i_seconds = seconds(s);
     }
 
-    // Before Start Settings
+
     bool add_5_minutes();
     void startTimer();
-    // While Timer Running Controls
-    void add_5_minutes_curr();
-    void toggle_timer(int type);
-    void toggle_break_timer();
+
+    std::pair<int,int> get_current_time();
+    bool is_finished();
 
 };
 
