b2f045b2cd12ccc6e76b152abdb339edb9f48838
duckly216 <jorgep020905@gmail.com>
2025-10-27 22:02:25 -0400

    Started implementation of get_current_time

--- src/Timer.cpp
+++ src/Timer.cpp
@@ -6,39 +6,39 @@
 #include "Timer.h"
 
 
+Timer::Timer(int m, int s) {
+    this->i_minutes = minutes(m);
+    this->i_seconds = seconds(s);
+    total_duration = i_minutes + i_seconds;
+}
+
 // Type determines whether it adds to work or break
-void Timer::add_5_minutes(int type) {
-    switch (type) {
-        case WORK:
-            work_timer += 5;
-            break;
-        case BREAK:
-            break_timer += 5;
-            break;
-        default: ;
-    }
+bool Timer::add_5_minutes() {
+    // Timer will only be able to display 2 digits for minutes
+    if (i_minutes >= minutes(95))
+        return false;
+    i_minutes += minutes(5);
+    return true;
 }
 void Timer::startTimer()
 {
-    int type = WORK; // initially will always be work
-    // timer will alternate between work and break times (cycles) number of times
-    for (int i = 0; i < cycles; i++) {
-        toggle_timer(type);
-        type = BREAK; // timer type switches to break
-        toggle_timer(type);
-    }
+    start_time = steady_clock::now();
 }
-
-void Timer::toggle_timer(int type) {
-    const int target_time = (type == BREAK) ? break_timer : work_timer;
-    string type_debug = (type == BREAK) ? "Break" : "Work";
-    current_timer = chrono::steady_clock::now() + chrono::minutes(target_time);
-    while (chrono::steady_clock::now() < current_timer) {
-        cout << type_debug <<" Time left: "
-         << chrono::duration_cast<chrono::minutes>(current_timer - chrono::steady_clock::now()).count()
-         << " m | "
-         << chrono::duration_cast<chrono::seconds>(current_timer - chrono::steady_clock::now()).count()%60
-         << " s\n";
-        this_thread::sleep_for(chrono::seconds(1));
-    }
+std::pair<int,int> Timer :: get_current_time(){
+    auto elapsed_time = steady_clock::now() - start_time;
+    auto remaining_time = total_duration - elapsed_time;
+    if (remaining_time.count() <= 0) return {0,0};
 }
+// void Timer::toggle_timer(int type) {
+//     const int target_time = (type == BREAK) ? break_timer : work_timer;
+//     string type_debug = (type == BREAK) ? "Break" : "Work";
+//     current_timer = chrono::steady_clock::now() + chrono::minutes(target_time);
+//     while (chrono::steady_clock::now() < current_timer) {
+//         cout << type_debug <<" Time left: "
+//          << chrono::duration_cast<chrono::minutes>(current_timer - chrono::steady_clock::now()).count()
+//          << " m | "
+//          << chrono::duration_cast<chrono::seconds>(current_timer - chrono::steady_clock::now()).count()
+//          << " s\n";
+//         this_thread::sleep_for(chrono::seconds(1));
+//     }
+// }
