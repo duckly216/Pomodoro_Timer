f7458cc3982c3e8c85ca8da4e8212247c52128bb
duckly216 <jorgep020905@gmail.com>
2025-11-09 16:07:04 -0500

    Created code to illustrate basic timer set up

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,30 @@
 //
 // Created by jorge on 10/20/2025.
 //
-#include "Timer.h"
-#include<iostream>
 
+#include<iostream>
+#include "Timer.h"
 using namespace std;
 
 int main() {
-    Timer timer(1,1,1);
-    timer.startTimer();
+    cout << "Welcome to Pomodoro Timer" << endl;
+    Timer the_timer;
+    the_timer.set_all(1,30);
+    char input;
+
+    the_timer.startTimer();
+    cout << "Timer started for " << 1 << ":" << 30 <<endl;
+    cout<<"This is where time happens "<<endl;
+    while(true) {
+        auto [curr_min, curr_sec] = the_timer.get_current_time();
+        cout << curr_min << ":" << (curr_sec < 10 ? "0" : "") << curr_sec << endl;
+
+        if(curr_min == 0 && curr_sec == 0) {
+            cout << "Timer Finished" << endl;
+            break;
+        }
+        this_thread::sleep_for(milliseconds(1000));
+    }
 
     // cout << "begin\n";
     // int time_in_min = 3;
