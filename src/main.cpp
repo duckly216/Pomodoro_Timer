2a55a63c66eedbdf006eaa3bf7cd04410e3fe582
duckly216 <jorgep020905@gmail.com>
2025-11-25 11:10:58 -0500

    Created test code to test the pomodoro logic

--- src/main.cpp
+++ src/main.cpp
@@ -9,21 +9,32 @@ using namespace std;
 int main() {
     cout << "Welcome to Pomodoro Timer" << endl;
     Timer the_timer;
-    the_timer.set_all(1,30);
+    the_timer.set_all(0,5);
+    Timer break_timer;
+    break_timer.set_all(0,8);
+
+    bool on_break = false;
+    int loops = 2;
     char input;
 
-    the_timer.startTimer();
-    cout << "Timer started for " << 1 << ":" << 30 <<endl;
-    cout<<"This is where time happens "<<endl;
-    while(true) {
-        auto [curr_min, curr_sec] = the_timer.get_current_time();
-        cout << curr_min << ":" << (curr_sec < 10 ? "0" : "") << curr_sec << endl;
+    for (int i = 1; i <= loops * 2; i++) {
+        cout<<"This is where "<< (on_break ? "break": "work") <<" time happens "<<endl;
+        Timer& focus = on_break ? break_timer : the_timer;
+        focus.startTimer();
+        while (true){
+            // cout << "Timer started for " << 0 << ":" << 05 <<endl;
 
-        if(curr_min == 0 && curr_sec == 0) {
-            cout << "Timer Finished" << endl;
-            break;
+
+            auto [curr_min, curr_sec] = focus.get_current_time();
+            cout << curr_min << ":" << (curr_sec < 10 ? "0" : "") << curr_sec << endl;
+
+            if(curr_min == 0 && curr_sec == 0) {
+                cout << "Timer Finished" << endl;
+                break;
+            }
+            this_thread::sleep_for(milliseconds(900));
         }
-        this_thread::sleep_for(milliseconds(1000));
+        on_break = !on_break;
     }
 
     // cout << "begin\n";
